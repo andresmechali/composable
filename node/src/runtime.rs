1a8defcf71a6e009c07e2f4d0cc6ec643194e55b
benluelo <benluelo@hotmail.com>
2022-04-18 15:17:10 -0400

    Add seperate traits for HostRuntimeApis per runtime

--- node/src/runtime.rs
+++ node/src/runtime.rs
@@ -3,14 +3,11 @@ use primitives::currency::CurrencyId;
 use sp_runtime::traits::BlakeTwo256;
 
 /// Consider this a trait alias.
-pub trait HostRuntimeApis:
+pub trait BaseHostRuntimeApis:
 	sp_transaction_pool::runtime_api::TaggedTransactionQueue<Block>
 	+ sp_api::ApiExt<Block>
 	+ sp_block_builder::BlockBuilder<Block>
 	+ substrate_frame_rpc_system::AccountNonceApi<Block, AccountId, Index>
-	+ pallet_transaction_payment_rpc_runtime_api::TransactionPaymentApi<Block, Balance>
-	+ assets_runtime_api::AssetsRuntimeApi<Block, CurrencyId, AccountId, Balance>
-	+ crowdloan_rewards_runtime_api::CrowdloanRewardsRuntimeApi<Block, AccountId, Balance>
 	+ sp_api::Metadata<Block>
 	+ sp_consensus_aura::AuraApi<Block, sp_consensus_aura::sr25519::AuthorityId>
 	+ sp_offchain::OffchainWorkerApi<Block>
@@ -22,15 +19,12 @@ where
 {
 }
 
-impl<Api> HostRuntimeApis for Api
+impl<Api> BaseHostRuntimeApis for Api
 where
 	Api: sp_transaction_pool::runtime_api::TaggedTransactionQueue<Block>
 		+ sp_api::ApiExt<Block>
 		+ sp_block_builder::BlockBuilder<Block>
 		+ substrate_frame_rpc_system::AccountNonceApi<Block, AccountId, Index>
-		+ pallet_transaction_payment_rpc_runtime_api::TransactionPaymentApi<Block, Balance>
-		+ assets_runtime_api::AssetsRuntimeApi<Block, CurrencyId, AccountId, Balance>
-		+ crowdloan_rewards_runtime_api::CrowdloanRewardsRuntimeApi<Block, AccountId, Balance>
 		+ sp_api::Metadata<Block>
 		+ sp_consensus_aura::AuraApi<Block, sp_consensus_aura::sr25519::AuthorityId>
 		+ sp_offchain::OffchainWorkerApi<Block>
@@ -38,5 +32,28 @@ where
 		+ cumulus_primitives_core::CollectCollationInfo<Block>
 		+ pallet_transaction_payment_rpc::TransactionPaymentRuntimeApi<Block, Balance>,
 	<Self as sp_api::ApiExt<Block>>::StateBackend: sp_api::StateBackend<BlakeTwo256>,
+{
+}
+
+#[cfg(feature = "dali")]
+pub trait DaliHostRuntimeApis:
+	pallet_transaction_payment_rpc_runtime_api::TransactionPaymentApi<Block, Balance>
+	+ assets_runtime_api::AssetsRuntimeApi<Block, CurrencyId, AccountId, Balance>
+	+ crowdloan_rewards_runtime_api::CrowdloanRewardsRuntimeApi<Block, AccountId, Balance>
+{
+}
+
+pub trait PicassoHostRuntimeApis:
+	pallet_transaction_payment_rpc_runtime_api::TransactionPaymentApi<Block, Balance>
+	+ assets_runtime_api::AssetsRuntimeApi<Block, CurrencyId, AccountId, Balance>
+	+ crowdloan_rewards_runtime_api::CrowdloanRewardsRuntimeApi<Block, AccountId, Balance>
+{
+}
+
+#[cfg(feature = "composable")]
+pub trait ComposableHostRuntimeApis:
+	pallet_transaction_payment_rpc_runtime_api::TransactionPaymentApi<Block, Balance>
+	+ assets_runtime_api::AssetsRuntimeApi<Block, CurrencyId, AccountId, Balance>
+	+ crowdloan_rewards_runtime_api::CrowdloanRewardsRuntimeApi<Block, AccountId, Balance>
 {
 }
