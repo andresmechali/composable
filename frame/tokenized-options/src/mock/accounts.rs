0aecaf529221e345ac6b0ce51967e47b8bb2968e
Nikolai Kuklin <nikolai@composable.finance>
2022-06-27 14:59:37 +0000

    Add account_id_from_u64 function for tests.

--- frame/tokenized-options/src/mock/accounts.rs
+++ frame/tokenized-options/src/mock/accounts.rs
@@ -1,26 +1,22 @@
-use hex_literal::hex;
 use sp_core::sr25519::{Public, Signature};
 use sp_runtime::traits::{IdentifyAccount, Verify};
+
 pub type AccountId = <<Signature as Verify>::Signer as IdentifyAccount>::AccountId;
 
-pub static ADMIN: Public =
-	Public(hex!("0000000000000000000000000000000000000000000000000000000000000000"));
-pub static ALICE: Public =
-	Public(hex!("0000000000000000000000000000000000000000000000000000000000000001"));
-pub static BOB: Public =
-	Public(hex!("0000000000000000000000000000000000000000000000000000000000000002"));
-pub static CHARLIE: Public =
-	Public(hex!("0000000000000000000000000000000000000000000000000000000000000003"));
-pub static DAVE: Public =
-	Public(hex!("0000000000000000000000000000000000000000000000000000000000000004"));
-pub static EVEN: Public =
-	Public(hex!("0000000000000000000000000000000000000000000000000000000000000005"));
+pub static ADMIN: AccountId = account_id_from_u64(0);
+pub static ALICE: AccountId = account_id_from_u64(1);
+pub static BOB: AccountId = account_id_from_u64(2);
+pub static CHARLIE: AccountId = account_id_from_u64(3);
+pub static DAVE: AccountId = account_id_from_u64(4);
+pub static EVEN: AccountId = account_id_from_u64(5);
 
-// 	pub type AccountId = u128;
-
-// 	pub static ADMIN: AccountId = 0;
-// 	pub static ALICE: AccountId = 1;
-// 	pub static BOB: AccountId = 2;
-// 	pub static CHARLIE: AccountId = 3;
-// 	pub static DAVE: AccountId = 4;
-// 	pub static EVEN: AccountId = 5;
+pub const fn account_id_from_u64(n: u64) -> AccountId {
+	let bytes_src = n.to_be_bytes();
+	let mut bytes_dst = [0u8; 32];
+	let mut k = 0;
+	while k < bytes_src.len() {
+		bytes_dst[k + 24] = bytes_src[k];
+		k += 1;
+	}
+	Public(bytes_dst)
+}
